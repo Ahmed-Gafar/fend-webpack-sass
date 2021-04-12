e6dfd91927d13a56c8a6aaf80fa7c447a18dfb9e
AhmedGafar <ahmed.gafar@pixelogicmedia.com>
2021-04-12 17:32:47 +0200

    created a funtion that generates a table in html

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,19 +1,41 @@
 function handleSubmit(event) {
     event.preventDefault()
 
-    // check what text was put into the form field
-    let formText = document.getElementById('name').value
+    let data = [['this is test', 'eqvgjhwbkjn fejfnkfrb kfe bckoufshofe hi fi we  ew dhrf  bd  er hewes  fr ew f gre d  tr rf ']]
+    var tbl = tableCreate(data);
 
-    Client.checkForName(formText)
+    var myElement = document.getElementById("results").appendChild(tbl);
+    console.log(myElement);
 
-    console.log("::: Form Submitted :::")
-    fetch('http://localhost:8081/test')
-    .then(res => {
-        return res.json()
-    })
-    .then(function(data) {
-        document.getElementById('results').innerHTML = data.message
-    })
+
+}
+
+// function that takes as an input the name and label to create a table with it's size
+function tableCreate(data){
+    var tbl  = document.createElement('table');
+    tbl.style.width  = '850px';
+    tbl.id  = 'table';
+    tbl.style.border = '1px solid black';
+
+    var tr = tbl.insertRow();
+    var td = tr.insertCell();
+    td.appendChild(document.createTextNode('Word'));
+    td.style.border = '2px solid black';
+    td.style.fontWeight = 'bold';
+    var td = tr.insertCell();
+    td.appendChild(document.createTextNode('entity'));
+    td.style.border = '2px solid black';
+    td.style.fontWeight = 'bold';
+
+    for(var i = 0; i < data.length ; i++){
+        var tr = tbl.insertRow();
+        for(var j = 0; j < 2; j++){
+            var td = tr.insertCell();
+            td.appendChild(document.createTextNode(data[i][j]));
+            td.style.border = '1px solid black';
+        }
+    }
+    return tbl
 }
 
 export { handleSubmit }
