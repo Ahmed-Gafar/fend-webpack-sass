bd8b89be6c643710dd7be70b838a8690481ed3b2
AhmedGafar <ahmed.gafar@pixelogicmedia.com>
2021-04-14 10:15:19 +0200

    herouko

--- src/server/index.js
+++ src/server/index.js
@@ -16,6 +16,13 @@ app.use(
 
 app.use(express.static("dist"));
 
+// if (process.env.NODE_ENV === "production"){
+//   app.use(express.static("build"));
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname,  "build", "index.html"));
+//   });
+// }
+
 app.get("/", function (req, res) {
   res.sendFile("dist/index.html");
 });
