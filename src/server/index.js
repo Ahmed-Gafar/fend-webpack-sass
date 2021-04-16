07d7613641a772816e758d919237beffa9af9dc9
AhmedGafar <ahmed.gafar@pixelogicmedia.com>
2021-04-16 19:40:33 +0200

    call from backend

--- src/server/index.js
+++ src/server/index.js
@@ -5,6 +5,9 @@ var cors = require("cors");
 const dotenv = require("dotenv");
 var FormData = require("form-data");
 const fetch = require("node-fetch");
+const axios = require('axios');
+const cheerio = require('cheerio');
+const fs = require('fs');
 
 dotenv.config();
 
@@ -25,9 +28,30 @@ let API_KEY = process.env.API_KEY;
 
 app.post("/api_response", async function (req, res) {
   let url = req.body.url;
+
+  let input = ''
+  await axios.get(url)
+    .then((response) => {
+        if (response.status === 200) {
+            const html = response.data;
+            const $ = cheerio.load(html);
+            // Get text 
+            console.log("------- with axios module -------")
+            console.log($.text());
+            input = $.text()
+
+            // Get HTML 
+            //console.log($.html());
+        }
+    })
+    .catch((err) => {
+        throw new Error(err);
+    });
+
+    console.log(input);
   const formdata = new FormData();
   formdata.append("key", process.env.API_KEY);
-  formdata.append("txt", url);
+  formdata.append("txt", input);
   formdata.append("lang", "en"); // 2-letter code, like en es fr ...
 
   const requestOptions = {
